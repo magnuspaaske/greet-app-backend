77364f56612c6691272e57cfe5abd26655498676
John Li <johnli611@gmail.com>
2015-02-28 21:27:14 +0100

    pseudocode in connect.js

---
+++ server/controllers/connect.js
@@ -0,0 +1,21 @@
+/* Takes in and compares two hash tables of format <user.accounts, isLoggedIn>. Any intersection  */
+
+var user1;
+var user2;
+
+var userList1 = user1.accounts; // self
+var userList2 = user2.accounts; // friend
+
+for (var i = 0; i < user1.accounts.length(), i++) {
+	if (userList1[i] == userList2[i]) {
+		if (userList[i] == true) {
+			connect(user1, user2, account);
+		}
+	}
+}
+
+/* access account's API for two users to connect over specified social network */
+
+function connect(User user1, User user2, Account account) {
+
+}
