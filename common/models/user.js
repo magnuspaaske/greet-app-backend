b893e66c103ff56d68a5b4ea74e2e7e128b57c06
Magnus Petersen-Paaske <magnuspetersenpaaske@gmail.com>
2015-03-01 12:29:33 +0100

    the very latest stuff

--- common/models/user.js
+++ common/models/user.js
@@ -1,17 +1,121 @@
 module.exports = function(User) {
-  User.me = function(cb) {
+  // Should send back all the data for the current user (not working yet)
+  User.me = function(custom, cb) {
+    var user = this;
     cb(null, {
-      "user": User.current
+      "foo": "bar",
+      "request": custom
     });
+    return custom;
   }
   User.remoteMethod(
     'me',
     {
       http: {path: '/me', verb: 'get'},
+      accepts: [
+        {
+          arg: 'custom',
+          type: 'object',
+          http: {
+            source: 'req'
+          }
+        }
+      ],
       returns: {arg: 'me', type: 'string'}
     }
   );
+
+  // simply mirrors whatever is posted to check http requests
+  User.mirror = function(x, y, z, callback) {
+    callback(null, {
+      "x": x,
+      "y": y,
+      "z": z
+    });
+  }
+  User.remoteMethod(
+    'mirror',
+    {
+      accepts: [
+        {arg: 'x', type: 'string'},
+        {arg: 'y', type: 'string'},
+        {arg: 'z', type: 'string'}
+      ],
+      http: {path: '/mirror', verb: 'post'},
+      returns: {arg: 'mirror', type: 'string'}
+    }
+  )
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
